14b0545590a8bc45d1f59850552835cc4ca5454a
yan <yanajiki2010@hotmail.com>
2024-06-04 14:53:16 -0400

    init repository

---
+++ src/main/java/com/yanajiki/srv/notifierhub/NotifierHubApplication.java
@@ -0,0 +1,13 @@
+package com.yanajiki.srv.notifierhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifierHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanajiki/srv/notifierhub/NotifierHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanajiki.srv.notifierhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifierHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
