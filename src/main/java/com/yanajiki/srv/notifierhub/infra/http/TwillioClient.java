13b3d7f9717d4c484c270947db4b2456fed41474
yan <yanajiki2010@hotmail.com>
2024-06-10 16:35:34 -0400

    feat: consuming TWILLIO API with success

--- src/main/java/com/yanajiki/srv/notifierhub/infra/http/TwillioClient.java
+++ src/main/java/com/yanajiki/srv/notifierhub/infra/http/TwillioClient.java
@@ -6,9 +6,12 @@ import com.yanajiki.srv.notifierhub.core.port.NotificationSender;
 import com.yanajiki.srv.notifierhub.infra.mongo.document.SenderConfigurationData;
 import com.yanajiki.srv.notifierhub.infra.mongo.repository.SenderConfigurationRepository;
 import feign.FeignException;
+import feign.Param;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.Base64;
 
 @Component
 @RequiredArgsConstructor
@@ -27,17 +30,18 @@ public class TwillioClient {
         var accountSID = authParts[0];
         var authToken = authParts[1];
         var twillioPhoneNumber = authParts[2];
-        var basicAuth = String.format("Basic %s:[%s]", accountSID, authToken);
+
+        var encodedAuthToken = String.format("%s:%s", accountSID, authToken);
+        var basicAuthToken = "Basic " + Base64.getEncoder().encodeToString(encodedAuthToken.getBytes());
 
         try {
             log.info("Consuming TWILLIO API.");
-            twillioFeignClient.sendMessage(
-                    accountSID,
-                    notification.receiver(),
-                    twillioPhoneNumber,
-                    notification.message(),
-                    basicAuth
-            );
+
+            var reqBody = String.format("To=%s&From=%s&Body='%s'",
+                    notification.receiver(), twillioPhoneNumber, notification.message());
+            twillioFeignClient.sendMessage(accountSID, reqBody, basicAuthToken);
+
+            log.info("Successfully consumed TWILLIO API.");
         } catch (FeignException.FeignClientException exception) {
             log.error("Failed to communicate with TWILLIO API.", exception);
         }
--- src/main/java/com/yanajiki/srv/notifierhub/infra/http/TwillioFeignClient.java
+++ src/main/java/com/yanajiki/srv/notifierhub/infra/http/TwillioFeignClient.java
@@ -2,21 +2,19 @@ package com.yanajiki.srv.notifierhub.infra.http;
 
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "twillioClient", url = "https://api.twilio.com/2010-04-01")
 public interface TwillioFeignClient {
 
     @PostMapping("/Accounts/{accountSID}/Messages.json")
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    void sendMessage(@RequestParam("accountSID") String accountSID,
-                     @RequestBody("To") String to,
-                     @RequestParam("From") String from,
-                     @RequestParam("Body") String message,
-                     @RequestHeader("Authorization") String basicAuth);
+//    @Headers("Content-Type: application/x-www-form-urlencoded")
+    void sendMessage(@PathVariable("accountSID") String accountSID,
+                     @RequestBody String urlEncodedReqBody,
+                     @RequestHeader("Authorization") String authToken);
 }
 
 
