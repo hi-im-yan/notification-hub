fb9807f75bad05bb181c3cb5ac9d1200b3b156f3
yan <yanajiki2010@hotmail.com>
2024-06-18 17:12:18 -0400

    doc: update documentation

--- src/main/java/com/yanajiki/srv/notifierhub/infra/http/dto/PushBulletResponse.java
+++ src/main/java/com/yanajiki/srv/notifierhub/infra/http/dto/PushBulletResponse.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Setter
 @Getter
+@ToString
 public class PushBulletResponse {
 
     private boolean active;
--- src/main/java/com/yanajiki/srv/notifierhub/infra/kafka/dto/NotificationDTO.java
+++ src/main/java/com/yanajiki/srv/notifierhub/infra/kafka/dto/NotificationDTO.java
@@ -10,6 +10,12 @@ import lombok.ToString;
 @ToString
 public class NotificationDTO {
     private String sender;
+    /*
+        In case that is a email, specify the receiver email.
+        In case that is a sms, specify the receiver number.
+        In case that is a pushbullet, specify the device_id.
+        In case that is a discord, specify the chat_id from the server that the bot is in.
+     */
     private String receiver;
     private String message;
     private String scheduledTime;
